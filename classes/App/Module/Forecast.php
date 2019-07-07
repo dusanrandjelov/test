f63e237c27667baca19beb57727209384eb691cc
dusanrandjelov <dusanrandjelov@gmail.com>
2019-07-07 22:42:31 +0200

    siteedit

--- classes/App/Module/Forecast.php
+++ classes/App/Module/Forecast.php
@@ -10,12 +10,43 @@ class Forecast extends Core {
 
     public function today(){
         $data = [
-            'title' => '',
-            'message' => ''
+            'title' => 'Vremenska prognoza',
+            'message' => 'Danasnja vremneska prognoza'
+        
         ];
 
         return $this->load_view('forecast/today', $data);
 
     }
-    
+    // public function fivedays(){
+    //     $data=[
+    //         'title' => 'Vremenska prognoza',
+    //         'message' => 'Vremneska prognoza za 5 dana'
+            
+    //     ];
+    //     return $this->load_view('forecast/fivedays',$data);
+    // }
+    // public function thirtydays(){
+    //     $data=[
+    //         'title' => 'Vremenska prognoza',
+    //         'message' => 'Vremneska prognoza za 30 dana'
+            
+    //     ];
+    //     return $this->load_view('forecast/thirtydays',$data);
+    // }
+    // public function seasonal(){
+    //     $data=[
+    //         'title' => 'Vremenska prognoza',
+    //         'message' => 'Sezonska vremenska prognoza',
+    //         'url' =>'http://localhost/test/classes/App/View/forecast/seasonal.phtml'
+    //     ];
+    //     return $this->load_view('forecast/seasonal',$data);
+    // }
+    // public function uvindex(){
+    //     $data=[
+    //         'title' => 'Vremenska prognoza',
+    //         'message' => 'UV Index'
+    //     ];
+    //     return $this->load_view('forecast/uvindex',$data);
+    // }
 }
